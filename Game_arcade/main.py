daa5a2eb104545d326e8910b795d0a6a8273a479
robiul161571 <143333941+robiul161571@users.noreply.github.com>
2023-09-27 11:10:58 +0600

    Add files via upload

---
+++ Game_arcade/main.py
@@ -0,0 +1,46 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
+screen = Screen()
+screen.tracer(0)
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("The Arcade Game")
+r_paddle = Paddle((360, 0))
+l_paddle = Paddle((-360, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+    # detect with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    # detect with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320  or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    # detect l_paddle mis
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    # detect r_paddle mis
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+
+
+screen.exitonclick()
